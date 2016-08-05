76df9f916065bba6a9dd36b74bef364a8b6a4325
Colin Ross <cross1@ltu.edu>
2016-08-05 09:55:09 -0400

    Initial Campaign
    (Commit)

---
+++ src/org/usfirst/frc/team5053/robot/Robot.java
@@ -0,0 +1,57 @@
+
+package org.usfirst.frc.team5053.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/*
+ * 
+ * RUDY FOR PRESIDENT 2016
+ * @Laker Robotics 2016 Summer Rewrite
+ * 
+ */
+
+public class Robot extends IterativeRobot {
+
+	/* Declare any and all variables here
+	 *  Do not initialize until robotInit()
+	 */
+	
+
+    public void robotInit() {
+        /**
+         * This function is run when the robot is first started up and should be
+         * used for any initialization code.
+         */
+    }
+
+    public void autonomousInit() {
+    	   /**
+         * This function is called once when autonomous begins
+         */
+    }
+
+    public void autonomousPeriodic() {
+
+        /**
+         * This function is called periodically during autonomous
+         */
+    }
+
+
+    public void teleopPeriodic() {
+        /**
+         * This function is called periodically during operator control
+         */
+    }
+
+    public void testPeriodic() {
+        
+        /**
+         * This function is called periodically during test mode
+         */
+    }
+    
+}
